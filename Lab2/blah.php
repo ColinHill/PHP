4707729a563d58e4f8484087dfa6fbe6fde1e45a
[Colin Hill] <[niloc_321@hotmail.com]>
2014-09-28 02:51:58 +0000

    Working on Part 3 - Lab 2

--- Lab2/blah.php
+++ Lab2/blah.php
@@ -18,16 +18,34 @@ if (isset($_POST['fname']) AND isset($_POST['lname']) AND isset($_POST['feet']) 
     $lname = $_POST['lname'];
     $feet = $_POST['feet'];
     $inches = $_POST['inches'];
+    $convertedHeight = ((($feet * 12) + ($inches)) * 0.0254);
 
     echo "<p>Your first name is: " . $fname . "</p>";
     echo "<p>Your last name is: " . $lname . "</p>";
     echo "<p>Your height in meters is: " . $convertedHeight . "</p>";
-}
+    echo "<hr>";
+    echo "<p>Tmp: ";
+    print_r($_FILES['UserFile']['tmp_name']);
+    echo "<p>Orig: ";
+    print_r($_FILES['UserFile']['name']);
+    echo "<p>Size: ";
+    print_r($_FILES['UserFile']['size']);
+    echo "<p>Error: ";
+    print_r($_FILES['UserFile']['error']);
+
+    $uploaddir = '/var/www/uploads/';
+    $uploadfile = $uploaddir . basename($_FILES['UserFile']['name']);
+
+      if (move_uploaded_file($_FILES['UserFile']['tmp_name'], $uploadfile)) {
+             echo "<p>File is valid, and was successfully uploaded.</p>";
+    } else {
+             echo "<p>The file did not upload.</p>";
+          }
+    }
 
 else
 {
     echo "<p>You must fill out all four fields.</p>";
-    echo     $convertedHeight = ((($feet * 12) + ($inches)) * 0.0254);
 }
 ?>
 <p><a href="blah.html">Go back.</a></p>
