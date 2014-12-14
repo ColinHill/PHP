34980f9cbdf58f5f09502ff3a17b6af6a830a2ea
[Colin Hill] <[niloc_321@hotmail.com]>
2014-12-14 22:23:59 +0000

    Started Highcharts Lab 9

---
+++ Lab9/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inet2005
+ * Date: 12/14/14
+ * Time: 10:16 PM
+ */
+?>
+
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/prototype/1.7/prototype.js"></script>
+    <script src="http://code.highcharts.com/adapters/prototype-adapter.js"></script>
+    <script src="http://code.highcharts.com/highcharts.js"></script>
+    <title>High Chart Tutorial</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
